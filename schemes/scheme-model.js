c605230f7e6606801a3b8bd880dd4314dd9f7b21
Matt Hagner <web@matthagner.com>
2019-08-27 20:10:59 -0500

    Implement findSteps function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,6 +15,13 @@ function findSteps(id) {
   return db
     .from("schemes")
     .innerJoin("steps", "schemes.id", "steps.scheme_id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .orderBy("steps.step_number")
     .where({ scheme_id: id });
 }
 
