04bb900e90dc3a77231668d578530047cdf1f25b
Matt Hagner <web@matthagner.com>
2019-08-27 19:34:25 -0500

    Scaffole controller file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,22 @@
+const db = require("../db");
+
+function find() {}
+
+function findById(id) {}
+
+function findSteps(id) {}
+
+function add(scheme) {}
+
+function update(changes, id) {}
+
+function remove(id) {}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
